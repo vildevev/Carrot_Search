d45115e516951e5321a330b9ccc47cf30f496e76
“Vilde <vilde@vevatne.no>
2017-09-18 16:45:47 -0700

    Add determine center positions method

--- Carrot_Search.rb
+++ Carrot_Search.rb
@@ -17,4 +17,17 @@ def find_possible_centers(size)
   size.odd? ? [size/2] : [(size/2)-1, size/2]
 end 
 
+def determine_center_position
+  values = Hash.new 
+  rows = find_possible_centers(@matrix.count)
+  columns = find_possible_centers(@matrix[0].count)
+  rows.each do |row| 
+    columns.each do |column| 
+      values[@matrix[row][column]] = [row, column]
+    end 
+  end 
+  @current_row = values.max_by{ |k,v| k }[1][0]
+  @current_column = values.max_by{ |k,v| k }[1][1]
+end 
+
 end 
