5071d5728e8047be9aa6421cbfc704ce5afada18
“Vilde <vilde@vevatne.no>
2017-09-18 17:10:05 -0700

    Add eat carrot methos

--- Carrot_Search.rb
+++ Carrot_Search.rb
@@ -17,7 +17,8 @@ def find_possible_centers(size)
   size.odd? ? [size/2] : [(size/2)-1, size/2]
 end 
 
-def determine_center_position
+# Find where to go based on which square has the highest value 
+def determine_position(rows, columns)
   values = Hash.new 
   rows = find_possible_centers(@matrix.count)
   columns = find_possible_centers(@matrix[0].count)
@@ -28,6 +29,13 @@ def determine_center_position
   end 
   @current_row = values.max_by{ |k,v| k }[1][0]
   @current_column = values.max_by{ |k,v| k }[1][1]
+end 
+
+# Adds value to carrot count array, also sets value of square to 0 
+def eat_carrot 
+	square = @matrix[@current_row][@current_column]
+	@carrot_count += square 
+	square = 0 
 end 
 
 end 
